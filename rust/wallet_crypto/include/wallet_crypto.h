0b451e8ae1632cd749155932ffc827fd6b95ad74
Junxin27 <tanjunxin27@gmail.com>
2026-04-27 13:32:03 +0800

    Update wallet_crypto.h

--- rust/wallet_crypto/include/wallet_crypto.h
+++ rust/wallet_crypto/include/wallet_crypto.h
@@ -1,1 +1,12 @@
+#pragma once
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+unsigned int rust_random_u32(void);
+unsigned char rust_sha256_first_byte(unsigned int input);
+
+#ifdef __cplusplus
+}
+#endif
